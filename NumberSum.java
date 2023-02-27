d3c5b87905d2a1fed481df55dd37d263567bbfdb
PratikParhad <pratkparhd@gmail.com>
2023-02-27 12:33:51 +0530

    added more codes for user interaction

--- NumberSum.java
+++ NumberSum.java
@@ -5,12 +5,13 @@ public class NumberSum{
     public static void main(String[] args){
         int num1 = 0;
         int num2 = 0;
+	System.out.println("Enter 2 Numbers to be added");
         System.out.println("Enter First Number");
         num1 = sc.nextInt();
         System.out.println("Enter Second Number");
         num2 = sc.nextInt();
 
         int result = num1+num2;
-        System.out.println("Sum is "+ result);
+        System.out.printf("Sum of %d & %d is : %d",num1 , num2, result);
     }
 }
